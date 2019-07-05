2802021ee45649a0a35ce9e252c030283352ea27
abdo hassan <abdocoder0@gmail.com>
2019-07-05 18:17:18 +0200

    styling + adding book list

---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+  return books.length ? (
+    <div className='book-list'>
+      <ul>
+        {books.map(book => {
+          return <BookDetails book={book} key={book.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className='empty'>No Books To Read...</div>
+  );
+};
+
+export default BookList;
